b269f37192813feca5cb526fab3dc4d320d13381
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-09-16 17:11:26 +0900

    [check] 文字列での動作を確認

--- app/src/main/java/com/wsr/lazycolumnstudy/List.kt
+++ app/src/main/java/com/wsr/lazycolumnstudy/List.kt
@@ -2,23 +2,27 @@ package com.wsr.lazycolumnstudy
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 
 @Composable
 fun List(){
     
-    val list = remember { listOf("A", "B", "C")}
+    val list by remember { mutableStateOf((0..10).toMutableList()) }
+    var item by remember { mutableStateOf("TEST") }
 
     LazyColumn(Modifier.fillMaxSize()){
         item{
             for(i in list){
-                LazyCard(text = i)
+                LazyCard(text = i.toString())
             }
+            LazyCard(text = item)
         }
         item { 
-            AddButton(onClick = { /*TODO*/ })
+            AddButton(onClick = {
+                list.add(list.size)
+                item = "OK"
+            })
         }
     }
 }
