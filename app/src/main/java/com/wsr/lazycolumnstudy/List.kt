d0e7cd058a47bc9ecd472327e3df661fbaeaa7a3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-09-16 21:17:06 +0900

    [add] listでの動作を確認ｎ

--- app/src/main/java/com/wsr/lazycolumnstudy/List.kt
+++ app/src/main/java/com/wsr/lazycolumnstudy/List.kt
@@ -1,5 +1,6 @@
 package com.wsr.lazycolumnstudy
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.*
@@ -8,20 +9,25 @@ import androidx.compose.ui.Modifier
 @Composable
 fun List(){
     
-    val list by remember { mutableStateOf((0..10).toMutableList()) }
-    var item by remember { mutableStateOf("TEST") }
+    var list = remember { mutableStateOf((0..3).toMutableList()) }
 
     LazyColumn(Modifier.fillMaxSize()){
         item{
-            for(i in list){
+            for(i in list.value){
                 LazyCard(text = i.toString())
             }
-            LazyCard(text = item)
         }
         item { 
             AddButton(onClick = {
-                list.add(list.size)
-                item = "OK"
+//                Log.d("Hello", (list + listOf(list.size)).toString())
+
+                val tempList = list.value
+                tempList.add(list.value.size)
+                Log.d("Hello", "list: ${list.value}")
+                Log.d("Hello", "tempList: $tempList")
+                Log.d("Hello", (list.value == tempList).toString())
+
+                list.value = tempList
             })
         }
     }
